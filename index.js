884b5f51860313f0f38e322c660f76f8a9a852b1
SamMaster08 <sgomezeliz@gmail.com>
2024-06-04 20:33:27 -0500

    segundo commit

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ class CRUD {
         this.idImput = document.getElementById("id");
         this.tableBody = document.getElementById("crud-table-body");
         this.loadItems();
-        this.from.addEventListener('submit', (e)=> this.saveIten(e)); //guarda lo del input en la variable
+        this.form.addEventListener('submit', (e)=> this.saveIten(e)); //guarda lo del input en la variable
     }
 
     loadItems(){
         const itemJSON = localStorage.getItem('items');
         let items = [];
-        if (ItemsJSON){
+        if (itemJSON){
             // try                                    catch
             // JSON.parse convierte a formato JSON // recibe los errore; guarde el error en e y lo muestra
             try {
@@ -55,39 +55,41 @@ class CRUD {
         this.locadItems();
     }
 
-    saveIten(e){
-        e.preventDefault();
-        const name = this.nameInput.value.trim();
-        const valor = this.valorInput.value.trim();
-        const id = this.idInput.value;
-        const items = JSON.parse(localStorage.getItem('items')) || [];
-        if (id === '') {
-            // Add new item
-            const newItem = new Item(Date.now(), name, valor);
-            items.push(newItem);
-        } else {
-            // Update existing item
-            const index = items.findIndex(item => item.id == id);
-            items[index].name = name;
-            items[index].valor = valor;
-        }
-        this.saveItems(items);
-        this.form.reset();
-    }
 
-    editItem(id) {
-        const items = JSON.parse(localStorage.getItem('items')) || [];
-        const item = items.find(item => item.id == id); // metodo find
-        this.nameInput.value = item.name;
-        this.valorImput.value = item.c=valor;
-        this.idInput.value = item.id;
-    }
 
-    deleteItem(id) {
-        let items = JSON.parse(localStorage.getItem('items')) || [];
-        items = items.filter(item => item.id != id); // metodo filter
-        this.saveItems(items);
-    }
+     saveIten(e){
+         e.preventDefault();
+         const name = this.nameInput.value.trim();
+         const valor = this.valorInput.value.trim();
+         const id = this.idInput.value;
+         const items = JSON.parse(localStorage.getItem('items')) || [];
+         if (id === '') {
+             // Add new item
+             const newItem = new Item(Date.now(), name, valor);
+             items.push(newItem);
+         } else {
+             // Update existing item
+             const index = items.findIndex(item => item.id == id);
+             items[index].name = name;
+             items[index].valor = valor;
+         }
+         this.saveItems(items);
+         this.form.reset();
+     }
+
+     editItem(id) {
+         const items = JSON.parse(localStorage.getItem('items')) || [];
+         const item = items.find(item => item.id == id); // metodo find
+         this.nameInput.value = item.name;
+         this.valorImput.value = item.c=valor;
+         this.idInput.value = item.id;
+     }
+
+      deleteItem(id) {
+         let items = JSON.parse(localStorage.getItem('items')) || [];
+         items = items.filter(item => item.id != id); // metodo filter
+         this.saveItems(items);
+     }
 };
 
 const crud = new CRUD();
